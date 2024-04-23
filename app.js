a53fdb097513954fab05f69a7d3c2ab75d9b3bd9
cipher <dilsurbet@hmail.com>
2024-04-23 23:26:22 +0500

    4oy_4dars_uy_ishi

---
+++ app.js
@@ -0,0 +1,51 @@
+// 1 vazifa
+
+// let k = 6;
+// let n = 5;
+
+// for (let i = 0; i < n; i++) {
+//     if (n > 0) {
+//         console.log(k)    
+//     }             
+// }
+
+// 2 vazifa
+
+// let A = parseInt(prompt("A raqamini kiriting"));
+// let B = parseInt(prompt("B raqamini kiriting"));
+// let count = B - A + 1;
+
+// for (let i = A; i <= B; i++) {
+//     if (B < A) {
+//         console.log("B raqam A raqamdan katta bo'lishi zarur!")
+//     } else {
+//         console.log(i);
+//     }    
+// }
+
+// console.log("Jami chop etuvchi sonlar " + count);
+
+// 3 vazifa
+
+// let A = 4;
+// let B = 8;
+// let count = B - A - 1;
+
+// while (B > A + 1) {
+//     B--;
+//     console.log(B);
+    
+// }
+// console.log("Жами чоп этувчи сонлар сони: " + count);
+
+// 4 vazifa
+
+// let konfet = parseInt(prompt("1 кг конфетни нархини киритинг:"));
+// let kg = parseInt(prompt("Неча килогача конфет олишингиз мумкин:"));
+
+// for (let i = 0; i < kg; i++) {
+//     console.log(`${i+1} kilo konfet jami ${konfet*(i+1)} so'm bo'ladi`)
+// }
+
+// 5 vazifa
+
