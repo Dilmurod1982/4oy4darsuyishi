af96d358bf94459b4f9197fc1ef391c6912e2bfa
cipher <dilsurbet@hmail.com>
2024-04-24 20:25:26 +0500

    yakun

--- app.js
+++ app.js
@@ -49,3 +49,91 @@
 
 // 5 vazifa
 
+// let price = parseInt(prompt("1 кг конфет нархини киритинг:"));
+// let kg = 0.1;
+
+// while (kg <= 1) {
+//     console.log(`${kg} кг: ${price * kg} ${" сўм"}`);
+//     kg += 0.1;
+// }
+
+// 6 vazifa
+
+// let price = parseInt(prompt("1 кг конфет нархини киритинг:"));
+// let kg = 1;
+
+// while (kg < 2.1) {
+//     console.log(`${kg} кг: ${price * kg} ${" сўм"}`);
+//     kg += 0.1;
+// }
+
+// 7 vazifa
+
+// let A = 10;
+// let B = 20;
+// let count = 0;
+
+// for (let i = A; i <= B; i++) {
+//     count +=i
+// }
+
+// console.log(count)
+
+// 8 vazifa
+
+// let A = 3;
+// let B = 8;
+// let C = 1;
+
+// for (let i = A; i < B; i++) {
+//     C *= A;
+//     A++    
+// }
+// console.log("Кўпайтма " + C);
+
+// 9 vazifa
+
+// let A = 3;
+// let B = 8;
+// let C = 0;
+
+// for (let i = A; i <= B; i++) {
+//     C += A * A;
+//     A++   
+    
+// }
+// console.log("Сонларнинг квадрат йиғиндиси " + C);
+
+// 10 vazifa
+
+// let A = parseInt(prompt("Бутун сон киритинг: "));
+// let S = 0
+
+// for (let i = 1; i <= A; i++) {
+//     S += 1 / i
+// }
+
+// console.log("Жами йиғинди: " + S)
+
+// 11 vazifa
+
+// let N = parseInt(prompt("Бутун сон киритинг: "));
+// let S = 0;
+
+// for (let i = 0; i <= N; i++) {
+//     S += (N + i) ** 2;
+// }
+
+// console.log("Жами: " + S);
+
+// 12 vazifa
+
+// let N = parseInt(prompt("Бутун сон киритинг: "));
+// let S = 1;
+
+// for (let i = 1.1; i <= N; i += 0.1) {
+//     S *= i;
+// }
+
+// console.log("Жами: " + S);
+
